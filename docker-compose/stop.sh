9973c25ac3ce45c8853ddcde6e2f08e1c07bac0a
郑标 <tanzhi.zheng@tuya.com>
2021-12-08 18:47:34 +0800

    简化 启动脚本

--- docker-compose/stop.sh
+++
@@ -1,8 +0,0 @@
-envFile=".env"
-if [[ ! -f "$envFile" ]]; then
-echo ".env not exist,use current config"
-cp -f ./config.properties ./.env
-fi
-sudo docker-compose down
-## 删除镜像使用以下指令
-### sudo docker-compose down --rmi all
